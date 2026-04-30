fa2037337f018a6c4ab367acf92d8e9b16c23ba3
Dan Henriksen <dhenrikse@bnl.gov>
2026-04-30 15:08:22 -0400

    make sure run_pdf() gets run during startup, added an acceptance test method

--- acceptance_tests/tests.py
+++ acceptance_tests/tests.py
@@ -6,7 +6,7 @@ def test_fly_scan2():
     """
     Fly scan test 2.
     If db.table() and export scan complete without errors than it was successful.
-    """    
+    """
     print("Starting fly scan test 2")
     uid, = RE(fly_scan(relative_rot_angle=20, out_x=10, out_y=20, out_z=30, simu=True))
     print("Fly scan complete")
@@ -30,6 +30,12 @@ def test_xanes_scan2():
     print("Exporting scan ...")
     export_scan(db[uid].start['scan_id'])
     print("Test is complete")
+
+def test_fly_scan3():
+    """
+    Fly scan test 3.
+    """
+    RE(fly_scan(exposure_time=0.05, start_angle=0, relative_rot_angle=180, period=0.05, out_x=None, out_y=-10, out_z=None, out_r=0, rs=6, relative_move_flag=True, rot_first_flag=1, filters=[], add_bkg_filt_only=False, rot_back_velo=30, binning=None, move_to_ini_pos=True, simu=True, take_bkg_img=True, take_dark_img=True, close_shutter_finish=True, note="None"))
 
 
 # ===========================================================================================
